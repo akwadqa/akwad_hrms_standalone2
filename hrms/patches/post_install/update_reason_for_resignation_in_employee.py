92adb601b5004c71c7a99db19473492183f496a7
Neamah Al-Safarjalani <neamah@akwad.qa>
2025-09-10 09:22:19 +0000

    Update setup.py to prepare for HRMS installation.

--- hrms/patches/post_install/update_reason_for_resignation_in_employee.py
+++ hrms/patches/post_install/update_reason_for_resignation_in_employee.py
@@ -6,12 +6,15 @@ import frappe
 
 
 def execute():
-	frappe.reload_doc("setup", "doctype", "employee")
+    if "erpnext" in frappe.get_installed_apps():
+        frappe.reload_doc("setup", "doctype", "employee")
+    else:
+        frappe.reload_doc("Stubs", "doctype", "employee")
 
-	if frappe.db.has_column("Employee", "reason_for_resignation"):
-		frappe.db.sql(
-			""" UPDATE `tabEmployee`
+    if frappe.db.has_column("Employee", "reason_for_resignation"):
+        frappe.db.sql(
+            """ UPDATE `tabEmployee`
             SET reason_for_leaving = reason_for_resignation
             WHERE status = 'Left' and reason_for_leaving is null and reason_for_resignation is not null
         """
-		)
+        )
