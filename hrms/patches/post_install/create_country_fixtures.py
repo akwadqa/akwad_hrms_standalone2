168a9a2fa736f83ecad08ecec6b7a622e1b42c03
Neamah Al-Safarjalani <neamah@akwad.qa>
2025-09-18 14:00:19 +0000

    Comment out post-install patches to allow successful HRMS installation.

--- hrms/patches/post_install/create_country_fixtures.py
+++ hrms/patches/post_install/create_country_fixtures.py
@@ -4,6 +4,7 @@ from hrms.overrides.company import make_salary_components, run_regional_setup
 
 
 def execute():
-	for country in frappe.get_all("Company", pluck="country", distinct=True):
-		run_regional_setup(country)
-		make_salary_components(country)
+	pass
+	# for country in frappe.get_all("Company", pluck="country", distinct=True):
+	# 	run_regional_setup(country)
+	# 	make_salary_components(country)
